a5484a209c82a134c844a0898409b8a89eaa326e
Manuel Haqi <mhscientist@gmail.com>
2019-08-23 21:29:42 +0200

    added items, selected cols, saved smaller csv

---
+++ ess_2019-08-23.R
@@ -0,0 +1,68 @@
+### European Social Survey
+
+
+# loading stuff -----------------------------------------------------------
+
+list.of.packages <- c("tidyverse",
+                      "plyr",
+                      "qgraph",
+                      "dplyr",
+                      "knitr",
+                      "kableExtra",
+                      "psych",
+                      "semPlot",
+                      "lavaan")
+new.packages <-
+  list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages, repos='http://cran.us.r-project.org')
+}
+tmp <- lapply(list.of.packages, require, character.only = TRUE)
+
+rm(list.of.packages, new.packages, tmp)
+# knitr::opts_chunk$set(echo = FALSE)
+
+# rm(list = ls())
+
+
+# selecting items and reading data ----------------------------------------------
+
+items <- list(
+  discrimination = c("dscrrce",
+                     "dscrntn",
+                     "dscrrlg",
+                     "dscrlng",
+                     "dscretn",
+                     "dscrage",
+                     "dscrgnd",
+                     "dscrsex",
+                     "dscrdsb",
+                     "dscroth"
+                     ),
+  
+  trust_social = c("ppltrst",
+                   "pplfair",
+                   "pplhlp"
+                   ),
+  
+  trust_political = c("trstprl",
+                      "trstlgl",
+                      "trstplc",
+                      "trstplt",
+                      "trstprt"
+                      ),
+  
+  political_hope = c("psppsgva",
+                     "actrolga",
+                     "psppipla",
+                     "cptppola"),
+  
+  political_interest = "polintr"
+  
+)
+
+# data.orig <- read.csv("https://raw.githubusercontent.com/psyguy/sem-course-project/master/data/ESS8e02.1_F1.csv")
+# data.orig <- read.csv("data/ESS8e02.1_F1.csv")
+# d <- data.orig %>% select(cntry, as.character(unlist(items)))
+# d %>% write.csv("data/ess2016_selected.csv", row.names = FALSE)
+

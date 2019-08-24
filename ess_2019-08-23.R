5d8a907f668966302e74f3648b375c5aeb309de9
Manuel Haqi <mhscientist@gmail.com>
2019-08-24 19:55:36 +0200

    added Epskamp example of colored semplot

--- ess_2019-08-23.R
+++ ess_2019-08-23.R
@@ -343,9 +343,48 @@ m.6 <- "
 
 "
 
+m.7 <- "
+
+# 1. latent variable definitions
+
+  hope_political =~ NA*psppsgva + actrolga + psppipla + cptppola
+  trust_social =~ NA*ppltrst + pplfair + pplhlp
+  trust_political =~ NA*trstprl + trstlgl + trstplc + trstplt + trstprt
+  # optimism_general =~ NA*trust_social + trust_political + hope_political
+  optimism_political =~ NA*psppsgva + actrolga + psppipla + cptppola +
+                        trstprl + trstlgl + trstplc + trstplt + trstprt
+
+# 2. regressions
+
+  hope_political + trust_political ~ polintr
+  hope_political + trust_social + trust_political + optimism_political ~ dscrscore
+
+# 3. (co)variances
+
+  hope_political ~~ 1*hope_political
+  trust_social ~~ 1*trust_social
+  trust_political ~~ 1*trust_political
+  # optimism_general ~~ 1*optimism_general
+  optimism_political ~~ 1*optimism_political
+  
+  hope_political ~~ trust_social + trust_political
+  trust_social ~~ trust_political
+
+# 4. intercepts
+  
+  psppsgva + actrolga + psppipla + cptppola ~ 1
+  ppltrst + pplfair + pplhlp ~ 1
+  trstprl + trstlgl + trstplc + trstplt + trstprt ~ 1
+
+# 5. thresholds
+  
+ # psppsgva + actrolga + psppipla + cptppola | t1 + t2 + t3 + t4
+
+"
+
 
 Sys.time()
-f <- lavaan(m.5, d,
+f <- lavaan(m.7, d,
             ordered = c(items_ordered, as.character(unlist(items$discrimination))),
             std.lv = TRUE,
             auto.var=TRUE)
@@ -373,6 +412,7 @@ semPaths(f,
          residuals=FALSE,
          fixedStyle=1,
          freeStyle=1,
+         style = "lisrel",
          # mar = c(1, 1, 1, 1),
          curvePivot = FALSE)
 
